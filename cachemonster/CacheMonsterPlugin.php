6b2bfab4c818a4af576d3194a74a7a1c32c6da61
Joris Noordermeer <jo.noordermeer@gmail.com>
2018-06-19 15:40:17 +0200

    Set Version number

--- cachemonster/CacheMonsterPlugin.php
+++ cachemonster/CacheMonsterPlugin.php
@@ -19,7 +19,7 @@ class CacheMonsterPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.4';
+        return '1.1.0';
     }
 
     public function getSchemaVersion()
